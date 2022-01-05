a202fb7144764e90a9c528da9dae25076a9cd86f
fachdf <fachridhia.f@gmail.com>
2022-01-05 07:17:23 +0700

    Up Dist

---
+++ frontend/dist/precache-manifest.68ada9e642f02a1894804d737a66b7fb.js
@@ -0,0 +1,366 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75468492b51776dc06ec",
+    "url": "/css/absensidosen.e24e5ad1.css"
+  },
+  {
+    "revision": "a4bd9e0b76144b39e5d7",
+    "url": "/css/absensimahasiswa.64d6c4d7.css"
+  },
+  {
+    "revision": "807f372bcfc48bcd7947",
+    "url": "/css/absensimahasiswadosenview.dfc68d4a.css"
+  },
+  {
+    "revision": "17e8e56ea07a4de6719a",
+    "url": "/css/add-new-user-by-form.54bcf9e8.css"
+  },
+  {
+    "revision": "5bef3ec9e94fe827fcc1",
+    "url": "/css/add-new-user.ae7eccb9.css"
+  },
+  {
+    "revision": "100eb00a2304975bb9d5",
+    "url": "/css/akun.1d5e3768.css"
+  },
+  {
+    "revision": "59ada7b4fbc5ac89724f",
+    "url": "/css/akun.login.7dbdf663.css"
+  },
+  {
+    "revision": "fa6a86a71d88c8ca649c",
+    "url": "/css/akun.reset-password.7c4cd8fa.css"
+  },
+  {
+    "revision": "74a2331a91a7bd1b9deb",
+    "url": "/css/alert-template.19138a97.css"
+  },
+  {
+    "revision": "1b56e0a8f1b6229f4427",
+    "url": "/css/app.d2d6048a.css"
+  },
+  {
+    "revision": "87d0a3f87baea6acf7b7",
+    "url": "/css/btn-template.9e6daffb.css"
+  },
+  {
+    "revision": "ef06f5798671ec2f1c43",
+    "url": "/css/chunk-05759136.f476c134.css"
+  },
+  {
+    "revision": "d38af7fc2bc0397b6077",
+    "url": "/css/chunk-0c15d082.edc1de41.css"
+  },
+  {
+    "revision": "7013ae1933c06d523ed4",
+    "url": "/css/chunk-11ce0731.cee127cc.css"
+  },
+  {
+    "revision": "2e7f6f86ab59ee371fa1",
+    "url": "/css/chunk-16889028.e02d5954.css"
+  },
+  {
+    "revision": "3ee75d2464db001daa1c",
+    "url": "/css/chunk-202619d3.f476c134.css"
+  },
+  {
+    "revision": "1e984369517af1a921b3",
+    "url": "/css/chunk-208d7d0c.f476c134.css"
+  },
+  {
+    "revision": "c016532838c396ebb261",
+    "url": "/css/chunk-20e54284.ab456b5c.css"
+  },
+  {
+    "revision": "41f41a3f6a726ebf1059",
+    "url": "/css/chunk-4f738a56.f476c134.css"
+  },
+  {
+    "revision": "df8f3572a5f4ca7815f7",
+    "url": "/css/chunk-64c52e92.a8b5ac21.css"
+  },
+  {
+    "revision": "2fc8564785ac476fa40c",
+    "url": "/css/chunk-652f118e.f476c134.css"
+  },
+  {
+    "revision": "7e346ff2f188cff6f558",
+    "url": "/css/chunk-vendors.b37ebc56.css"
+  },
+  {
+    "revision": "fdb6ff2584ef89063167",
+    "url": "/css/combobox-template.003e2f80.css"
+  },
+  {
+    "revision": "51c400838f8819ab55a0",
+    "url": "/css/dashboardlogbook.76afbd43.css"
+  },
+  {
+    "revision": "af7347dfa0cad5b2b3d8",
+    "url": "/css/inputnilaisection.e0a70fb4.css"
+  },
+  {
+    "revision": "36d9de49155c132a8a27",
+    "url": "/css/lihatlogbook.19ed4d9f.css"
+  },
+  {
+    "revision": "fb0c863c45cde656f52e",
+    "url": "/css/lihatlogbook~listLogbook~listmahasiswa~listmatakuliah~tambahlogbook~ubahlogbook~viewlogbook.8f2988ce.css"
+  },
+  {
+    "revision": "41325bcd8ff3f4b6e6e6",
+    "url": "/css/listLogbook.89e3522b.css"
+  },
+  {
+    "revision": "bab93166953e55ecc86c",
+    "url": "/css/listmahasiswa.6af994ac.css"
+  },
+  {
+    "revision": "8ece1a9111b097e0cdd5",
+    "url": "/css/listmatakuliah.991259e8.css"
+  },
+  {
+    "revision": "711ad2b937325a2fc94c",
+    "url": "/css/logbooksaya.ffa8127a.css"
+  },
+  {
+    "revision": "6df50802fd6f7c8dd563",
+    "url": "/css/paragraph-template.db78a1cb.css"
+  },
+  {
+    "revision": "7d29930b9fc9b233ab54",
+    "url": "/css/tambahlogbook.b77afff4.css"
+  },
+  {
+    "revision": "cbd3d2bf2c143402f168",
+    "url": "/css/textare-template.7407e2a4.css"
+  },
+  {
+    "revision": "3b4ca44bfd1f25ec8f83",
+    "url": "/css/textfield-template.9e6daffb.css"
+  },
+  {
+    "revision": "ba6e5e9575a0d6202d12",
+    "url": "/css/ubahlogbook.b77afff4.css"
+  },
+  {
+    "revision": "0be4a3eae8fed88b355b",
+    "url": "/css/uploadbukti.2c23d186.css"
+  },
+  {
+    "revision": "677b4ab078e71f239d79",
+    "url": "/css/viewlogbook.19ed4d9f.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/favicon1.ico"
+  },
+  {
+    "revision": "dd3be1bc5af628a7c086f5b9d38e80aa",
+    "url": "/img/1.dd3be1bc.png"
+  },
+  {
+    "revision": "3cd3be76c81e09bbaee2a780a8adc134",
+    "url": "/img/2.3cd3be76.png"
+  },
+  {
+    "revision": "bc88438dd1fd9af679ab6a21cb52bffa",
+    "url": "/img/3.bc88438d.png"
+  },
+  {
+    "revision": "1e2e9808efae8ed4bc5bdc9dd9dcdaa0",
+    "url": "/img/logout-card.1e2e9808.png"
+  },
+  {
+    "revision": "1982c491a2639a6719a228f64f613008",
+    "url": "/img/polban.1982c491.png"
+  },
+  {
+    "revision": "fa07ed4109bbf5871a5468886cc9ad78",
+    "url": "/img/sistem-absensi-card.fa07ed41.png"
+  },
+  {
+    "revision": "028d809df4a1b8c02657fc7fd2432a4c",
+    "url": "/img/sistem-penilaian-card.028d809d.png"
+  },
+  {
+    "revision": "2a6a7651a00a0ecb1149611ea4703374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75468492b51776dc06ec",
+    "url": "/js/absensidosen.38702492.js"
+  },
+  {
+    "revision": "a4bd9e0b76144b39e5d7",
+    "url": "/js/absensimahasiswa.c706df30.js"
+  },
+  {
+    "revision": "807f372bcfc48bcd7947",
+    "url": "/js/absensimahasiswadosenview.07b9b589.js"
+  },
+  {
+    "revision": "d70b6ca5bf713d7fa62b",
+    "url": "/js/add-new-user-.8221f55a.js"
+  },
+  {
+    "revision": "17e8e56ea07a4de6719a",
+    "url": "/js/add-new-user-by-form.c53cf915.js"
+  },
+  {
+    "revision": "5bef3ec9e94fe827fcc1",
+    "url": "/js/add-new-user.ca3198c4.js"
+  },
+  {
+    "revision": "100eb00a2304975bb9d5",
+    "url": "/js/akun.66f3756a.js"
+  },
+  {
+    "revision": "59ada7b4fbc5ac89724f",
+    "url": "/js/akun.login.85c7db0d.js"
+  },
+  {
+    "revision": "fa6a86a71d88c8ca649c",
+    "url": "/js/akun.reset-password.c5fc4e7b.js"
+  },
+  {
+    "revision": "74a2331a91a7bd1b9deb",
+    "url": "/js/alert-template.269b5630.js"
+  },
+  {
+    "revision": "1b56e0a8f1b6229f4427",
+    "url": "/js/app.9a23e438.js"
+  },
+  {
+    "revision": "87d0a3f87baea6acf7b7",
+    "url": "/js/btn-template.b0842636.js"
+  },
+  {
+    "revision": "ef06f5798671ec2f1c43",
+    "url": "/js/chunk-05759136.da4a2e2c.js"
+  },
+  {
+    "revision": "d38af7fc2bc0397b6077",
+    "url": "/js/chunk-0c15d082.386a7ac7.js"
+  },
+  {
+    "revision": "7013ae1933c06d523ed4",
+    "url": "/js/chunk-11ce0731.8e10af53.js"
+  },
+  {
+    "revision": "4fd4738c7357e6917738",
+    "url": "/js/chunk-1569fbf6.d6b55151.js"
+  },
+  {
+    "revision": "2e7f6f86ab59ee371fa1",
+    "url": "/js/chunk-16889028.c93ed500.js"
+  },
+  {
+    "revision": "3ee75d2464db001daa1c",
+    "url": "/js/chunk-202619d3.c5380df9.js"
+  },
+  {
+    "revision": "1e984369517af1a921b3",
+    "url": "/js/chunk-208d7d0c.60928e46.js"
+  },
+  {
+    "revision": "c016532838c396ebb261",
+    "url": "/js/chunk-20e54284.279cd683.js"
+  },
+  {
+    "revision": "4371f7fa8a1080647f49",
+    "url": "/js/chunk-2d216214.02f52b64.js"
+  },
+  {
+    "revision": "1b66c50051785da200f5",
+    "url": "/js/chunk-2d216257.d9b65536.js"
+  },
+  {
+    "revision": "41f41a3f6a726ebf1059",
+    "url": "/js/chunk-4f738a56.caedcff6.js"
+  },
+  {
+    "revision": "df8f3572a5f4ca7815f7",
+    "url": "/js/chunk-64c52e92.ef13d15c.js"
+  },
+  {
+    "revision": "2fc8564785ac476fa40c",
+    "url": "/js/chunk-652f118e.536c1df9.js"
+  },
+  {
+    "revision": "7e346ff2f188cff6f558",
+    "url": "/js/chunk-vendors.2a0d703d.js"
+  },
+  {
+    "revision": "fdb6ff2584ef89063167",
+    "url": "/js/combobox-template.6e32b6ca.js"
+  },
+  {
+    "revision": "51c400838f8819ab55a0",
+    "url": "/js/dashboardlogbook.7529314d.js"
+  },
+  {
+    "revision": "af7347dfa0cad5b2b3d8",
+    "url": "/js/inputnilaisection.0ba4df30.js"
+  },
+  {
+    "revision": "36d9de49155c132a8a27",
+    "url": "/js/lihatlogbook.17fce110.js"
+  },
+  {
+    "revision": "fb0c863c45cde656f52e",
+    "url": "/js/lihatlogbook~listLogbook~listmahasiswa~listmatakuliah~tambahlogbook~ubahlogbook~viewlogbook.a939a17b.js"
+  },
+  {
+    "revision": "41325bcd8ff3f4b6e6e6",
+    "url": "/js/listLogbook.0ce0c777.js"
+  },
+  {
+    "revision": "bab93166953e55ecc86c",
+    "url": "/js/listmahasiswa.a1401a03.js"
+  },
+  {
+    "revision": "8ece1a9111b097e0cdd5",
+    "url": "/js/listmatakuliah.54bdfef3.js"
+  },
+  {
+    "revision": "711ad2b937325a2fc94c",
+    "url": "/js/logbooksaya.481f5766.js"
+  },
+  {
+    "revision": "6df50802fd6f7c8dd563",
+    "url": "/js/paragraph-template.da0fec82.js"
+  },
+  {
+    "revision": "7d29930b9fc9b233ab54",
+    "url": "/js/tambahlogbook.8f0383e9.js"
+  },
+  {
+    "revision": "cbd3d2bf2c143402f168",
+    "url": "/js/textare-template.12a30551.js"
+  },
+  {
+    "revision": "3b4ca44bfd1f25ec8f83",
+    "url": "/js/textfield-template.698e3089.js"
+  },
+  {
+    "revision": "ba6e5e9575a0d6202d12",
+    "url": "/js/ubahlogbook.651f63ba.js"
+  },
+  {
+    "revision": "0be4a3eae8fed88b355b",
+    "url": "/js/uploadbukti.ef810585.js"
+  },
+  {
+    "revision": "677b4ab078e71f239d79",
+    "url": "/js/viewlogbook.979c5bdd.js"
+  },
+  {
+    "revision": "71463e0d9ef49c33b02ae4b29b329ea2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
